5d8b14d7858833db81b9081427f7fba96132de3c
Vinay Ram Gazula <vinayramgazula@gmail.com>
2025-11-20 22:28:19 -0500

    testing render deployment;

--- server.py
+++ server.py
@@ -18,5 +18,8 @@ mcp.add_tool(fetch_tables_in_schema)
 mcp.add_tool(fetch_table_info)
 mcp.add_tool(execute_spark_sql_query)
 
+# To deploy using uvicorn `uvicorn server:app --host 0.0.0.0 --port 8000`
+# app = mcp.streamable_http_app()
+
 if __name__ == "__main__":
     mcp.run(transport="streamable-http")
